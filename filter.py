43d71c98b7e02e01fdf78e32ec68c5ce01255053
vampirelhh <32383191+vampirelhh@users.noreply.github.com>
2018-12-01 23:36:52 +0800

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,66 @@
+import numpy as np
+import find_fixed_point
+
+def get_one_projector(Left_fixed_point, Right_fixed_point):
+    left = Left_fixed_point.copy()
+    right = Right_fixed_point.copy()
+    u, s, vh = np.linalg.svd(np.dot(left,right))
+    
+    # delete sigular values that are smaller than 1.e-12
+    length = len(s)
+    j = 0
+    for i in range(length):
+        
+        if(s[j]<1.0e-12):
+            u = np.delete(u,i,axis = 1)
+            vh = np.delete(vh,i,axis = 0)
+            s = np.delete(s,i,axis = 0)
+            j = j - 1
+        else:
+            j = j + 1   
+            
+    # get the square root and reciprocal of s
+    i = 0
+    modified_s = s
+    for i in range(len(s)):
+        modified_s[i] = np.sqrt(modified_s[i])
+        modified_s[i] = np.reciprocal(modified_s[i])
+        
+    # get all the matrices needed to compute projector    
+    modified_s = np.diag(modified_s)
+    uh = find_fixed_point.dagger(u)
+    v = find_fixed_point.dagger(vh)
+    
+    right_projector = np.dot(np.dot(right,v),modified_s)
+    left_projector  = np.dot(np.dot(modified_s,uh),left)
+    return left_projector,right_projector
+
+def filter(Tensor_A,Tensor_B):
+    D = Tensor_A.shape[0]
+    filter_tensor_A = Tensor_A.copy()
+    filter_tensor_B = Tensor_B.copy()
+    
+    #get all the fixed point
+    All_left_fixed_point = find_fixed_point.left_fixed_point(Tensor_A,Tensor_B,Tensor_A,Tensor_B,D)
+    All_right_fixed_point = find_fixed_point.right_fixed_point(Tensor_A,Tensor_B,Tensor_A,Tensor_B,D)
+    
+    #get all projectors
+    All_left_projector = [None]*4
+    All_right_projector = [None]*4
+    All_left_projector[0], All_right_projector[3] = get_one_projector(All_left_fixed_point[0],All_right_fixed_point[3])
+    All_left_projector[1], All_right_projector[0] = get_one_projector(All_left_fixed_point[1],All_right_fixed_point[0])
+    All_left_projector[2], All_right_projector[1] = get_one_projector(All_left_fixed_point[2],All_right_fixed_point[1])
+    All_left_projector[3], All_right_projector[2] = get_one_projector(All_left_fixed_point[3],All_right_fixed_point[2])
+    
+    #contract projector with original tensor
+    filter_tensor_A = np.einsum('mi,ijkl->mjkl',All_left_projector[0],filter_tensor_A)
+    filter_tensor_A = np.einsum('ijkl,jm->imkl',filter_tensor_A,All_right_projector[2])
+    filter_tensor_A = np.einsum('mk,ijkl->ijml',All_left_projector[2],filter_tensor_A)
+    filter_tensor_A = np.einsum('ijkl,lm->ijkm',filter_tensor_A,All_right_projector[0])
+    
+    filter_tensor_B = np.einsum('ijkl,im->mjkl',filter_tensor_B,All_right_projector[1])
+    filter_tensor_B = np.einsum('mj,ijkl->imkl',All_left_projector[1],filter_tensor_B)
+    filter_tensor_B = np.einsum('ijkl,km->ijml',filter_tensor_B,All_right_projector[3])
+    filter_tensor_B = np.einsum('ml,ijkl->ijkm',All_left_projector[3],filter_tensor_B)
+    
+    return filter_tensor_A, filter_tensor_B
---
+++ find_fixed_point.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+def contract_and_qr(r,tensor,D):
+    contract = np.einsum('ab,bcde->acde',r,tensor)
+    matrix = contract.reshape((D*D*D,D))
+    q, r = np.linalg.qr(matrix)
+    return r
+
+def rearrange_indice(T1,T2,T3,T4):
+    re_T1 = np.einsum('abcd->abcd',T1)
+    re_T2 = np.einsum('edfg->dfge',T2)
+    re_T3 = np.einsum('jkei->eijk',T3)
+    re_T4 = np.einsum('mnak->kmna',T4)
+    ts_rearranged = (re_T1,re_T2,re_T3,re_T4)
+    return ts_rearranged
+
+def left_one_circle(ts_rearranged,L_old,D):
+    old_L = L_old.copy()
+    for i in range(4):
+        new_L = contract_and_qr(old_L,ts_rearranged[i],D)
+        old_L = new_L
+    new_L = new_L/new_L.max()
+    return new_L
+
+def find_error(mat1,mat2,D):
+    error = 0.0
+    for i in range(D):
+        for j in range(D):
+            error = error + (mat1[i,j]-mat2[i,j])**2
+    return error
+
+def left_fixed_point(T1,T2,T3,T4,D):
+    ini_fixed_point = np.ones([D,D])
+    L_all = [ini_fixed_point,ini_fixed_point,ini_fixed_point,ini_fixed_point]
+    
+    for i in range(4):        
+        ts_original = (T1,T2,T3,T4)
+        epsilon = 1.e-10
+        error = 1.0
+        L_old = np.ones([D,D])
+        L_new = np.ones([D,D])
+        iteration = 0
+
+        ts_rearranged = rearrange_indice(T1,T2,T3,T4)
+        ts_rearranged_with_i = (ts_rearranged[(0+i)%4],ts_rearranged[(1+i)%4],
+                                ts_rearranged[(2+i)%4],ts_rearranged[(3+i)%4])
+
+        while(error > epsilon and iteration < 30):
+            L_new = left_one_circle(ts_rearranged_with_i,L_old,D)
+            error = find_error(L_old, L_new, D)
+            L_old = L_new
+            iteration = iteration + 1
+        L_all[i] = L_new
+        
+    All_left_points = (L_all[0],L_all[1],L_all[2],L_all[3])
+    
+    return All_left_points
+
+def contract_and_lq(l,tensor,D):
+    contract = np.einsum('abcd,de->abce',tensor,l)
+    matrix = contract.reshape((D,D*D*D))
+    matrix = matrix.transpose()
+    q, r = np.linalg.qr(matrix.conjugate())
+    r = r.transpose()
+    r = r.conjugate()
+    return r
+
+def right_one_circle(ts_rearranged,R_old,D):
+    old_R = R_old.copy()
+    for i in range(4):
+        new_R = contract_and_lq(old_R,ts_rearranged[-i],D)
+        old_R = new_R
+    new_R = new_R/new_R.max()
+    return new_R
+
+def right_fixed_point(T1,T2,T3,T4,D):
+    ini_fixed_point = np.ones([D,D])
+    R_all = [ini_fixed_point,ini_fixed_point,ini_fixed_point,ini_fixed_point]
+    
+    for i in range(4):        
+        ts_original = (T1,T2,T3,T4)
+        epsilon = 1.e-10
+        error = 1.0
+        R_old = np.ones([D,D])
+        R_new = np.ones([D,D])
+        iteration = 0
+
+        ts_rearranged = rearrange_indice(T1,T2,T3,T4)
+        ts_rearranged_with_i = (ts_rearranged[(0+i)%4],ts_rearranged[(1+i)%4],
+                                ts_rearranged[(2+i)%4],ts_rearranged[(3+i)%4])
+        while(error > epsilon and iteration < 50):
+            R_new = right_one_circle(ts_rearranged_with_i,R_old,D)
+            error = find_error(R_old, R_new, D)
+            R_old = R_new
+            iteration = iteration + 1
+        R_all[i] = R_new
+        
+    All_right_points = (R_all[0],R_all[1],R_all[2],R_all[3])
+    return All_right_points
+
+def dagger(matrix):
+    in_matrix = matrix.copy()
+    out_matrix = in_matrix.transpose()
+    out_matrix = out_matrix.conjugate()
+    return out_matrix
+
+def getvalue(i):
+    if (i==0):
+        value = -1.0
+    elif (i==1):
+        value = 1.0
+    return value
+
+def gettensor(belta):
+    T = np.ones([2,2,2,2])
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                for l in range(2):
+                    T[i,j,k,l] = np.exp(belta*(getvalue(i)*getvalue(j) + getvalue(j)*getvalue(k) +
+                                               getvalue(k)*getvalue(l) + getvalue(l)*getvalue(i)))
+    return T
