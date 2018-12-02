06cabbdb5867f26441fda35b699d4817c81fe311
Yue Zhengyuan <yuezy1997@icloud.com>
2018-12-02 10:49:09 +0800

    Verified result

--- main.py
+++ main.py
@@ -20,14 +20,22 @@ ts_TA[1,0,1,0] = np.exp(-4*beta)
 ts_TA[0,0,0,0] = np.exp(4*beta)
 ts_TA[1,1,1,1] = np.exp(4*beta)
 ts_TB = ts_TA.copy()
+
+# partition function for 4 sites
 result1 = np.einsum('ajkb,cbmj,mdci,kiad',ts_TA,ts_TB,ts_TA,ts_TB)
+# partition function for 8 sites
+result4 = np.einsum('mnfe,pqhg,heib,fgaj,ijkl,abcd,clmq,kdpn',ts_TA,ts_TA,ts_TB,ts_TB,ts_TA,ts_TA,ts_TB,ts_TB)
 
 # entanglement filtering
 ts_TA, ts_TB = flt.filter(ts_TA, ts_TB)
+# partition function for 4 sites (after filtering)
 result2 = np.einsum('ajkb,cbmj,mdci,kiad',ts_TA,ts_TB,ts_TA,ts_TB)
+# should have result1 = result2
 
 # loop optimize
 ts_TA, ts_TB = opt.loop_optimize((ts_TA,ts_TB), 16, 10E-10)
+# partition function for 4 sites after RG
 result3 = np.einsum('ajkb,cbmj,mdci,kiad',ts_TA,ts_TB,ts_TA,ts_TB)
+# should have result3 = result4
 
-print(result1,result2,result3)
+print(result1,result2,result3,result4)
